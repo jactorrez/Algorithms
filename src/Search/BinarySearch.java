5f90419ad4240604f795826a5973b5c15906d89c
Javier Torrez <javierctorrez@gmail.com>
2017-06-11 00:16:12 -0400

    implemented non-recursive (iterative) version of binary search

--- src/Search/BinarySearch.java
+++ src/Search/BinarySearch.java
@@ -11,6 +11,10 @@ public class BinarySearch {
 		return binarySearch(target, 0, data.length - 1);
 	}
 	
+	public boolean iterativeSearch(int target){
+		return binarySearchIterative(target);
+	}
+	
 	/* Returns true if the target value is found in the indicated portion of the array
 	 * This search only considers the array portion from data[low] to data[high] inclusive
 	 */
@@ -30,6 +34,24 @@ public class BinarySearch {
 		}
 	}
 	
+	/* Turns recursive binarySearch function non-recursive (iterative) */
+	protected boolean binarySearchIterative(int target){
+		int low = 0;
+		int high = data.length - 1;
+		while(low <= high){
+			int mid = (low + high)/2;
+			if(target == data[mid]){
+				return true;	
+			} else if(target < data[mid]){
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		
+		return false;
+	}
+	
 	public static void main(String[] args){
 		int[] testArr = {1,2,3,5,6,7,8,9};
 		BinarySearch test = new BinarySearch(testArr);
@@ -39,5 +61,8 @@ public class BinarySearch {
 		System.out.println(valueFound);
 		System.out.println("Look for number 4 (Should return false)");
 		System.out.println(valueNotFound);
+		System.out.println("Test non-recursive search function:");
+		System.out.println(test.iterativeSearch(2));
+		System.out.println(test.iterativeSearch(4));
 	}
 }
