10b0c5462c7154ee54f41660bdd3b795df44b0db
Javier Torrez <javierctorrez@gmail.com>
2017-06-10 22:24:34 -0400

    implemented a recursive method for reporting disk usage of a file system

---
+++ src/diskUsage.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+public class diskUsage {
+	public static long diskUsage(File root){
+		long total = root.length();						// start with direct disk usage
+		if(root.isDirectory()){							// check if this is a directory
+			for(String childName : root.list()){		// for each child...
+				File child = new File(root, childName); // compose full path to child
+				total += diskUsage(child);				// add child's usage to total
+			}
+		}
+		
+		System.out.println(total + "\t" + root);	// descriptive output
+		return total;								// return the grand total
+	}
+}
