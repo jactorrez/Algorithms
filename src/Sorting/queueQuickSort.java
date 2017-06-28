c2676ed56bf24f5384a19c0e370e1e675a5b28e9
Javier Torrez <javierctorrez@gmail.com>
2017-06-28 10:41:54 -0400

    implemented quick sort on a queue

---
+++ src/Sorting/queueQuickSort.java
@@ -0,0 +1,50 @@
+package Sorting;
+
+import java.util.Comparator;
+import Queue.*;
+
+public class queueQuickSort extends AbstractSort{
+	
+	public static <K> void quickSort(Queue<K> S, Comparator<K> comp){
+		int n = S.size();
+		if (n < 2){							// queue is trivially sorted
+			return;								
+		}
+		
+		K pivot = S.first();			    // using first as arbitrary pivot
+		
+		// divide original into L, E, and G
+		Queue<K> L = new LinkedQueue<>();
+		Queue<K> E = new LinkedQueue<>();
+		Queue<K> G = new LinkedQueue<>();
+		
+		while(!S.isEmpty()){
+			K element = S.dequeue();
+			int c = comp.compare(element, pivot);
+			if(c < 0){					   // element is less than pivot 
+				L.enqueue(element);
+			} else if(c == 0){
+				E.enqueue(element);        // element is equal to pivot
+			} else {
+				G.enqueue(element);        // element is greater than pivot
+			}
+		}
+		
+		// conquer
+		quickSort(L, comp);				  // sort elements less than pivot
+		quickSort(G, comp);				  // sort elements greater than pivot
+		
+		// concatenate results
+		while(!L.isEmpty()){
+			S.enqueue(L.dequeue());
+		} 
+		
+		while(!E.isEmpty()){
+			S.enqueue(E.dequeue());
+		}
+		
+		while(!G.isEmpty()){
+			S.enqueue(G.dequeue());
+		}
+	}
+}
