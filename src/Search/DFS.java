62751078d142bee4e6de5bc22a5d58251b8b11d1
Javier Torrez <javierctorrez@gmail.com>
2017-07-01 20:57:16 -0400

    implemented breatdth-first search for graph

--- src/Search/DFS.java
+++ src/Search/DFS.java
@@ -18,7 +18,7 @@ public class DFS {
 			Vertex<V> v = g.opposite(u, e);
 			if(!known.contains(v)){
 				forest.put(v, e);				// e is the tree edge that discovered v
-				DFSearch(g, v, known, forest);		// recursively explore from v 
+				DFSearch(g, v, known, forest);  // recursively explore from v 
 			}
 		}
 	}
@@ -38,5 +38,26 @@ public class DFS {
 			}	
 		}
 		return path;
+	}
+	
+	/* Performs breadth-first search of Graph g starting at Vertex u */
+	public static <V,E> void BFS(Graph<V,E> g, Vertex<V> s, Set<Vertex<V>> known, Map<Vertex<V>, Edge<E>> forest){
+		LinkedPositionalList<Vertex<V>> level = new LinkedPositionalList<>();
+		known.add(s);
+		level.addLast(s); 	// first level includes only s
+		while(!level.isEmpty()){
+			LinkedPositionalList<Vertex<V>> nextLevel = new LinkedPositionalList<>();
+			for(Vertex<V> u : level){
+				for(Edge<E> e : g.outgoingEdges(u)){
+					Vertex<V> v = g.opposite(u, e);
+					if(!known.contains(v)){
+						known.add(v);
+						forest.put(v,e);		// e is the tree edge that discovered v
+						nextLevel.addLast(v);	// v will be further considred in next pass
+					}
+				}
+				level = nextLevel;		     	// relabel 'next' level to become the current
+			}	
+		}
 	}
 }
