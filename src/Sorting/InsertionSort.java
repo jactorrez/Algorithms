fa8e4331ff617e8c1a3b2b480156867e3a3e1021
Javier Torrez <javierctorrez@gmail.com>
2017-08-28 23:05:09 -0400

    overloaded sorting algorithm to work with Lists

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -1,43 +1,65 @@
 package Sorting;
-import java.util.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InsertionSort{
 	
-	protected int[] array;
-	
-	public InsertionSort(int[] array){
-		this.array = array;
+	@SuppressWarnings("unchecked")
+	public static <T> boolean less(Comparable<T> v, Comparable<T> w){
+		return (v.compareTo((T) w) < 0);
 	}
 	
-	public String printArray(){
-		String arrToString = Arrays.toString(array);
-		return arrToString;
-	}
-	
-	public void sort(){
-		int arrLength = array.length;
+	public static <T> void sort(Comparable<T>[] arr){
 		
-		for(int i = 1; i < arrLength; i++){
+		int N = arr.length;
+		
+		for(int i = 1; i < N; i++){
 			int currentIndex = i;
-			int currentVal = array[i];
+			Comparable<T> currentVal = arr[i];
 			
-			while(currentIndex >= 1 && currentVal < array[currentIndex - 1]){
-				array[currentIndex] = array[currentIndex - 1];
+			while(currentIndex >= 1 && less(currentVal, arr[currentIndex - 1])){
+				arr[currentIndex] = arr[currentIndex - 1];
 				currentIndex--;
 			}
 			
-			array[currentIndex] = currentVal;
+			arr[currentIndex] = currentVal;
 		}
 	}
 	
+	public static <T> void sort(ArrayList<Comparable<T>> arr){
+		
+		int N = arr.size();
+		
+		for(int i = 1; i < N; i++){
+			int currentIndex = i;
+			Comparable<T> currentVal = arr.get(i);
+			
+			while(currentIndex >= 1 && less(currentVal, arr.get(currentIndex - 1))){
+				arr.set(currentIndex, arr.get(currentIndex - 1));
+				currentIndex--;
+			}
+			
+			arr.set(currentIndex, currentVal);
+		}
+	}
+	
+	
 	public static void main(String[] args){
-		int[] testArr = {9, 8, 7, 5, 3, 1, 0};
+		ArrayList<Comparable<Integer>> testArr = new ArrayList<>();
+		testArr.add(5);
+		testArr.add(4);
+		testArr.add(2);
+		testArr.add(90);
 		
-		InsertionSort test = new InsertionSort(testArr);
-		System.out.println("Before sort: " + test.printArray());
-		test.sort();
-		System.out.println("After sort: " + test.printArray());
+		InsertionSort.sort(testArr);
+	
+		System.out.print("[");	
+		for(Comparable<Integer> item : testArr){
 		
+			System.out.print(item + ",");
+		}
+		System.out.print("]");	
 	}
 }
 
